30216384377ac9734946b7133040b5314e508ad5
ElliotXLeo <elliotgaramendi@gmail.com>
2021-12-05 20:39:32 -0500

    Creación de README.md

--- base-de-datos/crud-b-dt-jq-php.sql
+++ base-de-datos/crud-b-dt-jq-php.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-08-2021 a las 10:49:04
--- Versión del servidor: 10.4.19-MariaDB
--- Versión de PHP: 8.0.7
+-- Tiempo de generación: 06-12-2021 a las 02:32:38
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,28 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `login_php_b_jq_sa2_i`
+-- Base de datos: `crud-b-dt-jq-php`
 --
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `centro_educativo`
---
-
-CREATE TABLE `centro_educativo` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `nivel` varchar(50) NOT NULL,
-  `ubicacion` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `centro_educativo`
---
-
-INSERT INTO `centro_educativo` (`id`, `nombre`, `nivel`, `ubicacion`) VALUES
-(1, 'Idat', 'Instituto', '@-12.0671257,-77.0358376,21z');
 
 -- --------------------------------------------------------
 
@@ -81,17 +61,22 @@ CREATE TABLE `usuario` (
 
 INSERT INTO `usuario` (`id`, `usuario`, `clave`, `celular`, `ubicacion`, `idRol`) VALUES
 (1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', '951123456', '@-12.0460077,-77.0305912,21z', 1),
-(2, 'demo', 'fe01ce2a7fbac8fafaed7c982a04e229', '985145753', '@-12.0460077,-77.0305912,21z', 2);
+(2, 'demo', 'fe01ce2a7fbac8fafaed7c982a04e229', '985145753', '@-12.0460077,-77.0305912,21z', 2),
+(3, 'ElliotXLeo', 'e10adc3949ba59abbe56e057f20f883e', '963640765', '@-12.0671257,-77.0358376,21z', 2),
+(4, 'Chocolate', '202cb962ac59075b964b07152d234b70', '951753456', '@-12.0671257,-77.0358376,21z', 2),
+(5, 'Doky', '202cb962ac59075b964b07152d234b70', '954613554', '@-12.0671257,-77.0358376,21z', 2),
+(6, 'Amorosa', '202cb962ac59075b964b07152d234b70', '948546132', '@-12.0671257,-77.0358376,21z', 2),
+(7, 'NN', '202cb962ac59075b964b07152d234b70', '954613213', '@-12.0671257,-77.0358376,21z', 2),
+(8, 'Chispa', '202cb962ac59075b964b07152d234b70', '985461321', '@-12.0671257,-77.0358376,21z', 2),
+(9, 'Patón', '202cb962ac59075b964b07152d234b70', '954613294', '@-12.0671257,-77.0358376,21z', 2),
+(10, 'Arthas', '202cb962ac59075b964b07152d234b70', '999781325', '@-12.0671257,-77.0358376,21z', 2),
+(11, 'Quilla', '202cb962ac59075b964b07152d234b70', '945613294', '@-12.0671257,-77.0358376,21z', 2),
+(12, 'Inti', '202cb962ac59075b964b07152d234b70', '933545464', '@-12.0671257,-77.0358376,21z', 2),
+(13, 'Gaaa', '202cb962ac59075b964b07152d234b70', '985461313', '@-12.0671257,-77.0358376,21z', 2);
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `centro_educativo`
---
-ALTER TABLE `centro_educativo`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `rol`
@@ -113,12 +98,6 @@ ALTER TABLE `usuario`
 --
 
 --
--- AUTO_INCREMENT de la tabla `centro_educativo`
---
-ALTER TABLE `centro_educativo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT de la tabla `rol`
 --
 ALTER TABLE `rol`
@@ -128,7 +107,7 @@ ALTER TABLE `rol`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Restricciones para tablas volcadas
--- frontend/index.php
+++ frontend/index.php
@@ -9,6 +9,7 @@
   <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css">
   <link rel="stylesheet" href="./css/index.css">
+  <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
 </head>
 
 <body class="body">
